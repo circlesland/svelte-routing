53afee1ea4654ea80629f48ea4ace81f234e33e8
Jitesh Doshi <jitesh@spinspire.com>
2020-01-26 21:20:47 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Router from "./Router.svelte";
+  import Route from "./Route.svelte";
+</script>
+
+<ul>
+  <li>
+    <a href="/">Home</a>
+  </li>
+  <li>
+    <a href="/foo">Foo</a>
+  </li>
+  <li>
+    <a href="/bar">Bar</a>
+  </li>
+</ul>
+
+<Router hashbang={false}>
+  <Route path="/">This is HOME page.</Route>
+  <Route path="/foo">This is FOO page.</Route>
+</Router>
